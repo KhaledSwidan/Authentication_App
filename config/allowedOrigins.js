0cf89c2271c72794b4dbdfbcea02ce829123c7b4
khaled swidan <khaledqutb4@gmail.com>
2025-04-20 23:03:43 +0200

    add new allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://authenticationapp-production-1c90.up.railway.app',
+];
 
 module.exports = allowedOrigins;
